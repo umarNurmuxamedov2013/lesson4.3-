f5b16b977da85ea69b769b03daaa4164bf90359e
umarNurmuxamedov2013 <nurmuxamedovumar@gmail.com>
2024-03-14 11:39:16 +0500

    men bu ni qilihsim sababi js o'rganmoqchiman

---
+++ main.js
@@ -0,0 +1,5 @@
+alert("Sizda hozir  random sonlar chiqishi boshlaydi !")
+let RandomSon =  Math.floor(Math.random() * 100)
+alert("Random orqali chiqqan son " + (RandomSon))
+alert("Biz random sonni 2ga bo'lib ko'paytirmoqchimiz")
+alert("Random orqali chiqqan son " + (RandomSon) + "\n" + "Biz uni 2ga ko'paytirib "  + (RandomSon * 2) + "\n" + "Biz uni 2ga bo'lganimizda " + (RandomSon / 2) + "\n" + "Va bu sonni 3ga bo'lganimizda qoldiq :" + (RandomSon % 3)) 
